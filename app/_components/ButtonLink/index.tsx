35225cefd55d58f535f594e4f38a9f991fba5971
eda6491 <eda6491@gmail.com>
2026-01-19 18:38:34 +0900

    3章まで完了

---
+++ app/_components/ButtonLink/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+type props = {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function ButtonLink({ href, children } : props) {
+  return(
+
+    <a href={href} className={styles.button}>{ children }</a>
+
+  );
+}
