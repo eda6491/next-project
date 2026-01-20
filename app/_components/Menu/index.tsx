772c5304e057407c31a86d4da106f7ca2b0642f0
eda6491 <241060977+eda6491@users.noreply.github.com>
2026-01-20 19:32:05 +0900

    4章まで完了

---
+++ app/_components/Menu/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Menu() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const toggle = () => setOpen(!isOpen);
+
+  return (
+    <div>
+      <nav className={styles.nav} id="navigation" aria-hidden={!isOpen}>
+        <ul className={styles.items}>
+          <li>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li>
+            <Link href="/member">メンバー</Link>
+          </li>
+          <li>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
+      <button
+        type="button"
+        aria-controls="navigation"
+        aria-expanded={isOpen}
+        className={styles.button}
+        onClick={toggle}
+      >
+        {isOpen ? (
+          <Image src="/close.svg" alt="閉じる" width={24} height={24} />
+        ) : (
+          <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+        )}
+      </button>
+    </div>
+  );
+}
