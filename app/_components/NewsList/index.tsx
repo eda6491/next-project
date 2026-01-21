29e7e4b2083db739210ef30aeb5a4c66e51f969f
eda6491 <241060977+eda6491@users.noreply.github.com>
2026-01-21 17:20:59 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.css";
-
 import Category from "../Category";
 import Date from "../Date";
 import { News } from "@/app/_libs/microcms";
@@ -21,13 +20,23 @@ export default function NewsList({ news }: Props) {
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
           <Link href={`/news/${article.id}`} className={styles.link}>
-            <Image
-              src="/no-image.png"
-              alt=""
-              className={styles.image}
-              width={1200}
-              height={630}
-            />
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                src="/no-image.png"
+                alt="No Image"
+                className={styles.image}
+                width={1200}
+                height={630}
+              />
+            )}
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
